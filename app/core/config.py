1fb7489dfcd834ee515bc530455221db1e3a25c1
Christopher Leon <christopherleon@192.168.68.108>
2025-11-24 12:18:19 -0600

    Feat: Increase token expiration to 10 years

--- app/core/config.py
+++ app/core/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     # JWT settings
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 8  # 8 hours
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 365 * 10  # 10 years
 
     # Webhook settings
     VERIFY_TOKEN: str
